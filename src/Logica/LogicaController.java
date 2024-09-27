1d165971ce7f55db41d02863bda678a60886e177
salo <salomontilla@outlook.com>
2024-09-27 01:20:21 -0500

    crea funciones seleccion y ejecutarSeleccion

--- src/Logica/LogicaController.java
+++ src/Logica/LogicaController.java
@@ -1,16 +1,67 @@
 package Logica;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LogicaController {
+    int seleccion;
     public void mostrarMenu(){
-        System.out.println("****** Menu *******");
-        System.out.println("1) De dolar (USD) a Peso Argentino (ARS)");
-        System.out.println("2) De Peso Argentino (ARS) a dolar (USD)");
-        System.out.println("3) De dolar (USD) a Real Brasileño (BRL)");
-        System.out.println("4) De Real Brasileño (BRL) a dolar (USD)");
-        System.out.println("5) De dolar (USD) a Peso Colombiano (COP)");
-        System.out.println("6) De Peso Colombiano (COP) a dolar (USD)");
-        System.out.println("7) Salir");
+        while(true){
+            System.out.println("****** Menu *******");
+            System.out.println("1) De dolar (USD) a Peso Argentino (ARS)");
+            System.out.println("2) De Peso Argentino (ARS) a dolar (USD)");
+            System.out.println("3) De dolar (USD) a Real Brasileño (BRL)");
+            System.out.println("4) De Real Brasileño (BRL) a dolar (USD)");
+            System.out.println("5) De dolar (USD) a Peso Colombiano (COP)");
+            System.out.println("6) De Peso Colombiano (COP) a dolar (USD)");
+            System.out.println("7) Salir");
+            seleccion = seleccion();
+            if(seleccion == 7){
+                System.out.println("Saliendo...");
+                break;
+            }else{
+                ejecutarSeleccion(seleccion);
+            }
+        }
+
+
     }
+    private int seleccion(){
+        System.out.println("**************\nIngrese una opcion: ");
+        Scanner sc = new Scanner(System.in);
+        try{
+            return sc.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("Ingrese un numero valido!\n");
+            mostrarMenu();
+            return 0;
+        }
+    }
+    private void ejecutarSeleccion(int opcion){
+        switch(opcion){
+            case 1:
+                System.out.println("Hola");
+                break;
+            case 2:
+                System.out.println("hola2");
+                break;
+            case 3:
+                System.out.println("hola3");
+                break;
+            case 4:
+                System.out.println("hola4");
+                break;
+            case 5:
+                System.out.println("hola5");
+                break;
+            case 6:
+                System.out.println("hola6");
+                break;
+            default:
+                System.out.println("Ingresa un valor entre 1 y 7!");
+                break;
+        }
+    }
+
+
 }
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,7 +1,10 @@
 package Principal;
 
+import Logica.LogicaController;
+
 public class Principal {
     public static void main(String[] args) {
-
+        LogicaController lg = new LogicaController();
+        lg.mostrarMenu();
     }
 }
